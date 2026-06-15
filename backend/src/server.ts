9e7c5ae431cb160735766e9cc37454030ea839b9
Lucas Macarini <contatolucasmacarini@gmail.com>
2026-06-15 08:53:13 -0300

    feat: add create method

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,6 +18,27 @@ app.get("/eventos", async (_req, res) => {
   res.json(eventos);
 });
 
+app.post("/eventos", async (req, res) => {
+  const { torneio, dataInicio, dataFim, local } = req.body;
+
+  if (!torneio || !dataInicio) {
+    return res
+      .status(400)
+      .json({ error: "Torneio e data de início são obrigatórios" });
+  }
+
+  try {
+    const evento = await prisma.evento.create({
+      data: { torneio, dataInicio, dataFim, local },
+    });
+    res.status(201).json(evento);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Erro ao criar evento", details: error });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
