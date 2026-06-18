b4c688fee38673ea279beb7a855474f1e8a49f3e
Lucas Macarini <contatolucasmacarini@gmail.com>
2026-06-18 00:32:01 -0300

    some

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,13 @@ import eventoRouter from "./routes/eventos";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./docs/swagger";
 import { errorHandler } from "./middleware/errorHandler";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
