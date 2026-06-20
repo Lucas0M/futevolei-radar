a4bb0aa1ac310919c8896afe72cd526c46788276
Lucas Macarini <contatolucasmacarini@gmail.com>
2026-06-20 14:33:42 -0300

    feat: Deployment on render and vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: ["http://localhost:5173", process.env.FRONTEND_URL!] }));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
